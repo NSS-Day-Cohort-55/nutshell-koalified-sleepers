21956a18c1a642a89b8ec3a657a96bfcf85c0901
karlarn <karlarnichols@gmail.com>
2022-04-19 15:08:28 -0500

    fixed footer to bottom of screen

--- src/components/nav/Footer.js
+++ src/components/nav/Footer.js
@@ -1,8 +1,10 @@
+import "./Footer.css"
+
 export const FooterCard = () => {
     return <>
-      <div id="container">
+      <footer id="container">
         <h2>Nutshell: Koalafied Sleepers &copy; {new Date().getFullYear()}</h2>
     <h4>Were not Facebook, stop asking.</h4>  
-      </div>
+      </footer>
        </>
   };
